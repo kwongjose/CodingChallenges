b66b7e8e56487d61e4a8337d18bb6a105336d817
Kwong <joseph.kwong@tsi.corp.com>
2020-02-07 10:55:15 -0600

    added comments to Rotate Array solutions

--- Rotate Array/C#/Rotate-Array-Solution-1.cs
+++ Rotate Array/C#/Rotate-Array-Solution-1.cs
@@ -1,17 +1,48 @@
 public class Solution {
+    /// <summary>Rotates an Array k places to the right using a placeholder Array</summary>
+    /// <param name='nums'>The Array to shift values right</param>
+    /// <parm name='k'>The number of places to shift the Array right</param>
     public void Rotate(int[] nums, int k) {
+        // Create a new Array to hold values that have been shifted right.
+        // This will be used to update the nums Array
         int[] left = new int[nums.Length];
         
         for(int idx = 0; idx < nums.Length; idx++){
-            if(idx+k < nums.Length){
-                left[idx+k] = nums[idx];
+            /*
+                If idx + k is in range the value is shifted right but not off the array.
+                Place the value from nums[index] into left[index + k] shifting it right
+
+                ex:
+                    nums[0, 1, 2, 3, 4], left[null, null, null, null, null]
+                    k = 2; idx = 1
+                    k + idx = 3
+                    left[3] = nums[1]
+                    nums[0, 1, 2, 3, 4], left[null, null, null, 1, null]
+            */
+            if(idx + k < nums.Length) {
+                left[idx + k] = nums[idx] ;
             }
-            else{
-                left[(idx+k)%nums.Length] = nums[idx];
+            else {
+                /*
+                    If idx + k is greater than the length of the nums array the value
+                    at nums[index] is shifted right past the end of the nums Array.
+                    Take the mod of  idx + k and the length of nums to find the index at the start of the left array
+                    that the nums value has been shifted to
+
+                ex:
+                    nums[0, 1, 2, 3, 4], left[null, null, null, null, null]
+                    k = 2; idx = 4
+                    k + idx = 6
+                    6 % 5 = 1;
+                    left[1] = nums[4]
+                    nums[0, 1, 2, 3, 4], left[null, 4, null, null, null]
+                */
+                left[(idx + k) % nums.Length ] = nums[idx];
             }
         }
+        // Loop over left and update nums with the new shifted order
         for(int i = 0; i < left.Length; i++){
-        nums[i] = left[i];
+            nums[i] = left[i];
         }
     }
 }
--- Rotate Array/JavaScript/Rotate-Array-solution-1.js
+++ Rotate Array/JavaScript/Rotate-Array-solution-1.js
@@ -4,5 +4,9 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    nums.splice(0, 0, ...nums.splice(nums.length - k, k));
+    /**
+     * Remove the last Kth items from end of the Array
+     * Add the removed items to the start of the Array
+     */
+    nums.splice(0, 0, ...nums.splice(nums.length - k, k) );
 };

a2f9452471db6c646fe7e50d6cfdff69526328d4
Kwong <joseph.kwong@tsi.corp.com>
2020-02-10 10:43:14 -0600

    fixed code being in wrong files

--- Detect Overlaping Intervals/JavaScript/DetectOverlaping-solution-1.js
+++ Detect Overlaping Intervals/JavaScript/DetectOverlaping-solution-1.js
@@ -1,3 +1,29 @@
 function overlap(arr){
+  let max_el  = 0;
 
+  for(let i = 0; i < arr.length; i++){
+      const [ start ] = arr[i];
+
+      if(max_el < start){
+          max_el = start;
+      }
   }
+
+  let aux = new Array(max_el + 1);
+  aux.fill(0, 0, aux.length);
+
+  for(let i = 0; i < arr.length; i++){
+    const [start, end ] = arr[i];
+    aux[start]++;
+    aux[end]--;
+  }
+
+  for(let i = 1; i <= max_el; i++){
+    aux[i] += aux[i - 1];
+
+    if(aux[i] > 1){
+      return true;
+    }
+  }
+
+}

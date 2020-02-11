48233dcfd90c716377025cdd98893665acab780c
Kwong <joseph.kwong@tsi.corp.com>
2020-02-11 09:01:33 -0600

    comments for python mergeIntervals and detect overlaping JS

--- Detect Overlaping Intervals/JavaScript/DetectOverlaping-solution-1.js
+++ Detect Overlaping Intervals/JavaScript/DetectOverlaping-solution-1.js
@@ -1,29 +1,48 @@
+/**
+ * Detects if a passed array has overlapping intervals
+ * Start and end  values of intervals are stored in an aux array
+ * The aux array is summed to check for overlapping. 
+ * @param {Array} arr - Array of intervals eg: [[1, 3], [4, 6]]
+ * @returns {Boolean} True if the intervals overlap
+ */
 function overlap(arr){
   let max_el  = 0;
 
+  // Find the max start value in the array
   for(let i = 0; i < arr.length; i++){
-      const [ start ] = arr[i];
+      const [ start, end ] = arr[i];
 
-      if(max_el < start){
-          max_el = start;
+      if(max_el < end){
+          max_el = end;
       }
   }
 
-  let aux = new Array(max_el + 1);
+  // make a Array the size of the max value
+  let aux = new Array(max_el + 2);
+  // Fill with 0s
   aux.fill(0, 0, aux.length);
 
+  // loop over the array. 
   for(let i = 0; i < arr.length; i++){
+    // Get the start and end of a interval
     const [start, end ] = arr[i];
+    // Increment start indexes in the aux Array
+    // Decrement end indexes in the aux Array
     aux[start]++;
-    aux[end]--;
+    aux[end + 1]--;
   }
-
+  // arr: [ [1, 3], [5, 9], [ 7, 12 ] ]
+  // aux: [0, 1, 0, -1, 0, 1, 0, 1, 0, -1, 0, 0, -1]
+  // Loop over the aux array
   for(let i = 1; i <= max_el; i++){
+    // add the index of the aux index with the previous index value
     aux[i] += aux[i - 1];
-
+    // Of the value is over 1 there is an overlap
     if(aux[i] > 1){
       return true;
     }
   }
+
+  return false;
 
 }
--- Merge Intervals/Python/MergeIntervals-solution-1.py
+++ Merge Intervals/Python/MergeIntervals-solution-1.py
@@ -5,27 +5,40 @@
 #         self.end = e
 
 class Solution(object):
+    """
+        Mergers overlaping intervals.
+        Sorts the interval list
+        Generates a new interval list passing in intervals that do not over lap
+        If intervals overlap merge the overlaping intervals and add to the interval list
+    """
     def merge(self, intervals):
         """
         :type intervals: List[Interval]
         :rtype: List[Interval]
         """
+        # sort the list of interval
         intervals.sort(key=lambda x: x.start)
         newInterval = []
         if len(intervals) > 0 :
             start = None
             end = 0
+            # loop over intervals in the list
             for interval in intervals:
-            
+                # If Start is not set assign start and end to the interval start and end
                 if start == None:
                     start = interval.start
                     end = interval.end
-                
+                # If end is greater or equal the interval start
+                # Assign end to the end of the interval unless it is equal
                 if end >= interval.start:
                     end = end if end > interval.end else interval.end
                 else:
+                    # if end is less than the start of the interval
+                    # Add a new interval with the current start and end
+                    # Update start and end with the values of the current interval
                     newInterval.append(Interval(start,end))
                     start = interval.start
                     end = interval.end
+            # Add the last interval to the new list
             newInterval.append(Interval(start,end))  
         return newInterval

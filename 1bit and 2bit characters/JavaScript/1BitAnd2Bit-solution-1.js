0ba1a6c74c0c66fc54922b9ae97969a72c168347
Kwong <joseph.kwong@tsi.corp.com>
2020-02-12 09:38:22 -0600

    1bit and 2 bit charatuer

---
+++ 1bit and 2bit characters/JavaScript/1BitAnd2Bit-solution-1.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} bits - Character bits eg: [1, 1, 0, 1, 0, 0]
+ * @return {boolean} If the last bit is a single bit bit character
+ */
+var isOneBitCharacter = function(bits) {
+    // loop over all bits in array
+    for(let i = 0; i < bits.length; i++){
+        let bit = bits[i];
+        // If the bit is 1 it must be part of a 2 bit character
+        if(bit === 1 ){
+            // Increment i so we skip the next bit
+            i++;
+            // If i is now the last index in the array the last bit is no a single bit character
+            if(i >= bits.length - 1){
+                return false;
+            }
+        }
+    }
+    return true
+};

7886a17ca77932cd5b42d0ab1be3e2a472b6eb5e
Kwong <joseph.kwong@tsi.com>
2020-02-19 09:45:20 -0600

    added sheet column javascript

---
+++ Sheet Column Titles/JavaScript/SheetColumnTitles-solution-1.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number} n - The number of the column title to find the name of
+ * @return {string} The column title eg: 1-> A 27 -> AA
+ */
+var convertToTitle = function(n) {
+    // Array to map the number to letters to
+    const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    // Array to hold letters
+    let str = [];
+    // If n is less than 26 just return the single letter
+    if(n <= 26) {
+        // Offset by 1 since Array starts at 0
+        return letters[n - 1];
+    } else {
+        let num = n;
+        // Loop until num is 0
+        while(num != 0){
+            // divide num and then get the mod
+            let quotient = Math.floor(num / 26);
+            let remainder = num % 26;
+            // If num is 26 or less add the letter then break since there are no more letters to find
+            if(num <= 26) {
+                str.push( letters[num - 1]);
+                break;
+            } else {
+                // If r is 0 we want to add Z otherwise we want to add r - 1
+                // Assign d to be the new num
+                if(remainder === 0) {
+                    str.push('Z');
+                     // If r is zero decrement - 1 from  d
+                    num = quotient - 1;
+                } else {
+                    str.push(letters[remainder - 1]);
+                    num = quotient;
+                }
+                str.push(letter);
+            }
+        }
+    }
+    // reverse the array and join to get the string of the column title
+    return str.reverse().join('');
+    
+};
